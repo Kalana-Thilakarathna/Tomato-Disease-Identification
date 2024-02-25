3cef8f5a796b5d398319a793b0ac78c55e3d3744
Kalana Thilakarathna <93968473+Kalana-Thilakarathna@users.noreply.github.com>
2024-02-25 19:35:37 +0530

    webapp finalization

--- Frontend/potato/src/Home.jsx
+++ Frontend/potato/src/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Box, Container, Typography } from "@mui/material";
 import { FileUploader } from "react-drag-drop-files";
 import React, { useEffect, useState } from "react";
@@ -5,6 +6,7 @@ import "./Home.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Placeholder from "react-bootstrap/Placeholder";
+import Spinner from "react-bootstrap/Spinner";
 
 import axios from "axios";
 
@@ -12,6 +14,11 @@ function Home() {
   const [file, setFile] = useState();
   const [fileurl, setFielurl] = useState();
   const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function Loader() {
+    return <Spinner animation="border" variant="success" />;
+  }
 
   const sendFile = async () => {
     if (file) {
@@ -20,12 +27,16 @@ function Home() {
       formData.append("file", file);
 
       try {
-        let res = await axios.post("https://us-central1-optimal-shard-410016.cloudfunctions.net/predict", formData);
+        let res = await axios.post(
+          "https://us-central1-optimal-shard-410016.cloudfunctions.net/predict",
+          formData
+        );
         console.log("file sent");
 
         if (res.status === 200) {
           setResults(res.data);
-          console.log(results)
+          console.log(results);
+          setLoading(false);
         }
       } catch (error) {
         console.error("Error sending file:", error);
@@ -34,6 +45,7 @@ function Home() {
   };
 
   useEffect(() => {
+    setLoading(true);
     sendFile();
   }, [file]);
 
@@ -70,10 +82,18 @@ function Home() {
           alignItems: "center",
           // Set the height to 100% of viewport height
           flexDirection: "column", // Align items vertically
-          marginTop:30,
+          marginTop: 30,
         }}
       >
-        <Card style={{ width: "30rem", height: "20rem" , display: 'flex', flexDirection: 'column', alignItems: 'center'  }}>
+        <Card
+          style={{
+            width: "30rem",
+            height: "20rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
           <Card.Body>
             <Box>
               <FileUploader
@@ -85,37 +105,41 @@ function Home() {
                 hoverTitle="drop here"
               />
               {fileurl && (
-                <div>
-                  {/* <img
-                    src={fileurl}
-                    alt="File Preview"
-                    style={{
-                      maxWidth: "100%",
-                      maxHeight: "300px",
-                      margin: "10px auto",
-                      display: "block",
-                    }}
-                  /> */}
-
-                  <Typography
-                    variant="subtitle1"
-                    sx={{ marginTop: 2, textAlign: "center" }}
-                  >
-                    Disease : {results["class_name"]}
-                    <br />
-                    confidence : {Math.floor(results.confidance * 1000) / 1000}
-                  </Typography>
+                <div style={{ height: "100%" }}>
+                  {loading ? (
+                    <div className="loader">
+                      <Loader />
+                    </div>
+                  ) : (
+                    <Typography
+                      variant="subtitle1"
+                      sx={{ marginTop: 2, textAlign: "center" }}
+                    >
+                      Disease : {results["class_name"]}
+                      <br />
+                      confidence :{" "}
+                      {Math.floor(results.confidance * 1000) / 1000}
+                    </Typography>
+                  )}
                 </div>
               )}
             </Box>
           </Card.Body>
-          <Card.Img
+          {fileurl ? (
+            <Card.Img
             variant="top"
             src={fileurl}
-            style={{ width: "150px", height: "150px"}}
+            style={{ width: "150px", height: "150px" }}
             alt="Image Alt Text"
             className="p-3"
           />
+          ):(
+            <div className="pre">
+            <h5>Image Preview</h5>
+
+            </div>
+          )}
+          
         </Card>
       </Container>
     </>
